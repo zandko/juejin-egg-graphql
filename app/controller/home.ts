d4dfd407d8677b25289115b1aec81bef09897347
gaozan <root@LAPTOP-LI328TRL.localdomain>
2019-12-30 09:51:14 +0800

    jwt

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -1,4 +1,5 @@
 import { Controller } from 'egg';
+import * as JWT from 'jsonwebtoken';
 
 export default class HomeController extends Controller {
   public async index() {
@@ -8,6 +9,22 @@ export default class HomeController extends Controller {
     //   scope: config.github.scope,
     // };
     // await this.ctx.render('login.html', data);
-    await this.ctx.render('socket.html');
+    const token = JWT.sign({
+      id: 1,
+      username: 'priase',
+    }, 'private.key', { expiresIn: 60 });
+
+    // console.log(token)
+
+    const verify = JWT.verify(token, 'private.key');
+    this.ctx.body = verify;
+    // decode = JWT.verify(token, options.secret);
+    // if (!decode || !decode.userName) {
+    //   ctx.throw(401, '没有权限，请登录');
+    // }
+    // if (Date.now() - decode.expire > 0) {
+    //   ctx.throw(401, 'Token已过期');
+    // }
+    // await this.ctx.render('socket.html');
   }
 }
